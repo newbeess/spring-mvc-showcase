ba5e2245298c073c129e577c9a8ba9cd832e88e0
c_fu <c_fu@Ctrip.com>
2017-07-18 10:04:11 +0800

    format code

--- src/main/java/org/springframework/samples/mvc/data/custom/CustomArgumentController.java
+++ src/main/java/org/springframework/samples/mvc/data/custom/CustomArgumentController.java
@@ -1,7 +1,6 @@
 package org.springframework.samples.mvc.data.custom;
 
 import javax.servlet.http.HttpServletRequest;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,14 +10,16 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class CustomArgumentController {
 
-	@ModelAttribute
-	void beforeInvokingHandlerMethod(HttpServletRequest request) {
-		request.setAttribute("foo", "bar");
-	}
-	
-	@RequestMapping(value="/data/custom", method=RequestMethod.GET)
-	public @ResponseBody String custom(@RequestAttribute("foo") String foo) {
-		return "Got 'foo' request attribute value '" + foo + "'";
-	}
+  @ModelAttribute
+  void beforeInvokingHandlerMethod(HttpServletRequest request) {
+    request.setAttribute("foo", "bar");
+  }
+
+  @RequestMapping(value = "/data/custom", method = RequestMethod.GET)
+  public
+  @ResponseBody
+  String custom(@RequestAttribute("foo") String foo) {
+    return "Got 'foo' request attribute value '" + foo + "'";
+  }
 
 }
